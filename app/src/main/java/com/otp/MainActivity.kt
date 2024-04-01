638cee397f9cf416805eba1ba493ae232c350a34
Shivam Shrivastava <43011013+SS7186805@users.noreply.github.com>
2024-04-01 17:39:22 +0530

    Ready to publish

--- app/src/main/java/com/otp/MainActivity.kt
+++ app/src/main/java/com/otp/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             OtpTheme {
-                    OtpView(type = OtpViewType.Round){
+                    OtpView(type = OtpViewType.Circle){
 
                     }
                 }
